2049b08ecbaafd3c944794338538f7a4e48ee9c2
131gus <131gus@gmail.com>
2022-09-28 18:06:01 +0900

    vuetify test

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/xxx",
+    name: "xxx",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/xxxView.vue"),
+  },
 ];
 
 const router = new VueRouter({
